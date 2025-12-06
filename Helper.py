55d3825a34981d5b6d7ec980659626dc1eaf9960
Tausif Hossain <>
2025-12-05 20:31:01 -0700

    helper function to print results added

--- Helper.py
+++ Helper.py
@@ -31,3 +31,30 @@ def route_distance(route, depot_index, dist_matrix):
         prev = node
     total += dist_matrix[prev][depot_index]
     return total
+
+def print_results(no_of_vehicles, total_dist, total_time, routes, weights, dist_matrix, capacity):
+    print("\nOutputs:")
+    print(f"Number of vehicles used: {no_of_vehicles}")
+    print(f"Total distance: {total_dist}")
+    print(f"Time taken to process = {total_time} milliseconds")
+    print("Routes:")
+    for r_idx, route in enumerate(routes, start=1):
+        # Compute load of this route (ignore depots)
+        route_load = sum(weights[i] for i in route)
+
+        # adding the depot in the beginning if needed
+        if route[0] != 0:
+            route = [0] + route
+
+        # adding the depot at last if needed
+        if route[len(route) - 1] != 0:
+            route = route + [0]
+
+        # Compute distance of this route
+        route_dist = 0.0
+        for a, b in zip(route, route[1:]):
+            route_dist += dist_matrix[a][b]
+
+        print(f"  Route {r_idx}: {' -> '.join(map(str, route))}")
+        print(f"    Load: {route_load} / {capacity}")
+        print(f"    Distance: {route_dist}")
